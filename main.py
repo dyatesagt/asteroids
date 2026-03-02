27597421133141b9bb3e781be9ce15d2a781a6c6
dyatesagt <dyates@agtrax.com>
2026-03-02 16:43:54 -0600

    B: Added player class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 from constants import (
     SCREEN_WIDTH, 
@@ -18,15 +19,35 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()    
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    Player.containers = (updatable, drawable)
+
+
+    player = Player(x, y)    
+    
     while True:
         log_state()
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-            screen.fill("black")
-            pygame.display.flip()
+                        
+        screen.fill("black")
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
